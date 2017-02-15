06f57c44e5155a7e4b231cec1c92f51ea3cb53fa
Feihong Hsu <feihong.hsu@gmail.com>
2017-02-15 09:47:13 -0600

    Set up sidebar

--- site/js/cms.js
+++ site/js/cms.js
@@ -1,6 +1,15 @@
 const app = new Vue({
   el: '#app',
   data: {
-    category: 'lyrics'
+    category: 'lyrics',
+    docs: [],
+    workingCopy: null
+  },
+  mounted() {
+    console.log('mounted')
+    axios.get(`/api/category/${this.category}/`).then(res => {
+      console.log(res)
+      this.docs = res.data
+    })
   }
 })
